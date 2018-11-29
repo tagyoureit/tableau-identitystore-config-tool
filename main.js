c10e6b717d3b8e9a17cc19ac265d9014fbe53688
tagyoureit <russ.goldin@gmail.com>
2018-11-29 10:59:31 -0800

    changes

--- main.js
+++ main.js
@@ -66,11 +66,30 @@
 
         var vals = idstoreform.domain.value.split('.')
         console.log(vals)
+        var formatted = "dc=" + vals[0] + ",dc=" + vals[1]
+        idstoreform.root.value = formatted
+        $("#rootnote").html("(If not " + formatted + " enter correct root below.)")
 
-        idstoreform.root.placeholder = "If not dc=" + vals[0] + ",dc=" + vals[1] + " then specify your root entry."
+        userform.userBaseDn.value = "o=users," + formatted
+        $("#userBaseDnNote").html('If not ' + userform.userBaseDn.value + " input appropriate value.  Must include root entry.")
+        groupform.groupBaseDn.value = "o=groups," + formatted
+        $("#groupBaseDnNote").html('If not ' + groupform.groupBaseDn.value + " input appropriate value.  Must include root entry.")
+
     })
 
-    $('#output').html(JSON.stringify(openLdapSimpleBindTemplate,null,2))
+    $('#idstoreform input[name=communicationsType]').on('change', function (e) {
+        console.log(e.currentTarget.value)
+        if (e.currentTarget.value === 'port') {
+            idstoreform.portNumber.value = 389
+        }
+        else {
+            idstoreform.portNumber.value = 636
+        }
+    })
+
+
+
+    $('#output').html(JSON.stringify(openLdapSimpleBindTemplate, null, 2))
 
     // Enable all tooltips
 
@@ -90,48 +109,46 @@
         }
         else {
             //ssl
-            openLdapSimpleBindTemplate.configEntities.identityStore.port = idstoreform.portNumber.value
+            openLdapSimpleBindTemplate.configEntities.identityStore.port = ""
             openLdapSimpleBindTemplate.configEntities.identityStore.sslPort = idstoreform.portNumber.value
         }
         openLdapSimpleBindTemplate.configEntities.identityStore.username = idstoreform.username.value
         openLdapSimpleBindTemplate.configEntities.identityStore.password = idstoreform.password.value
         console.log('Results after Basic Connection is completed.')
         console.log(openLdapSimpleBindTemplate)
-        $('#output').html(JSON.stringify(openLdapSimpleBindTemplate,null,2))
+        $('#output').html(JSON.stringify(openLdapSimpleBindTemplate, null, 2))
         $('#kerberos-tab').tab('show')
     })
 
-    $("#kerberosFormSubmit").on('click', function(){
-        openLdapSimpleBindTemplate.configEntities.kerberosConfig = kerberosform.kerberosConfig.value
+    $("#kerberosFormSubmit").on('click', function () {
+        // openLdapSimpleBindTemplate.configEntities.kerberosConfig = kerberosform.kerberosConfig.value
         openLdapSimpleBindTemplate.configEntities.kerberosPrincipal = kerberosform.kerberosPrincipal.value
-        openLdapSimpleBindTemplate.configEntities.kerberosKeytab = kerberosform.kerberosKeytab.value
+        // openLdapSimpleBindTemplate.configEntities.kerberosKeytab = kerberosform.kerberosKeytab.value
         console.log('Results after Kerberos Information is completed.')
         console.log(openLdapSimpleBindTemplate)
-        $('#user-tab').tab('show') 
-        $('#output').html(JSON.stringify(openLdapSimpleBindTemplate,null,2))
-})
+        $('#user-tab').tab('show')
+        $('#output').html(JSON.stringify(openLdapSimpleBindTemplate, null, 2))
+    })
 
 
-    $('#kerberosform input[name=enableKerberos]').on('change', function(e){
+    $('#kerberosform input[name=enableKerberos]').on('change', function (e) {
         console.log(e)
         console.log(e.currentTarget.value)
-        if (e.currentTarget.value==='Yes')
-        {
+        if (e.currentTarget.value === 'Yes') {
             $('#kerberosEnabledGroup').show()
-            openLdapSimpleBindTemplate.configEntities.identityStore.bind="simple"
+            openLdapSimpleBindTemplate.configEntities.identityStore.bind = "simple"
         }
-        else 
-        {
+        else {
             $('#kerberosEnabledGroup').hide()
-            openLdapSimpleBindTemplate.configEntities.identityStore.bind="gssapi"
-            openLdapSimpleBindTemplate.configEntities.kerberosConfig = ''
+            openLdapSimpleBindTemplate.configEntities.identityStore.bind = "gssapi"
+            // openLdapSimpleBindTemplate.configEntities.kerberosConfig = ''
             openLdapSimpleBindTemplate.configEntities.kerberos = ''
-            openLdapSimpleBindTemplate.configEntities.kerberosKeytab = ''
+            // openLdapSimpleBindTemplate.configEntities.kerberosKeytab = ''
         }
     })
 
-    $('#userFormSubmit').on('click', function(){
-        if (userform.userBaseDn.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.userBaseDn = userform.userBaseDn.value 
+    $('#userFormSubmit').on('click', function () {
+        if (userform.userBaseDn.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.userBaseDn = userform.userBaseDn.value
         if (userform.userBaseFilter.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.userBaseFilter = userform.userBaseFilter.value
         if (userform.userClassNames.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.userClassNames = userform.userClassNames.value
         if (userform.userdnAttribute.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.userdnAttribute = userform.userdnAttribute.value
@@ -141,16 +158,16 @@
         if (userform.userCertificate.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.userCertificate = userform.userCertificate.value
         if (userform.userThumbnail.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.userThumbnail = userform.userThumbnail.value
         if (userform.userJpegPhoto.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.userJpegPhoto = userform.userJpegPhoto.value
-        if (userform.userMemberOf.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.memberOf = userform.userMemberOf.value
+        // if (userform.userMemberOf.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.memberOf = userform.userMemberOf.value
         console.log('Results after User Information is completed.')
         console.log(openLdapSimpleBindTemplate)
-        $('#output').html(JSON.stringify(openLdapSimpleBindTemplate,null,2))
-        $('#group-tab').tab('show') 
+        $('#output').html(JSON.stringify(openLdapSimpleBindTemplate, null, 2))
+        $('#group-tab').tab('show')
     })
 
 
-    $('#groupFormSubmit').on('click', function(){
-        if (groupform.groupBaseDn.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.groupBaseDn = groupform.groupBaseDn.value 
+    $('#groupFormSubmit').on('click', function () {
+        if (groupform.groupBaseDn.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.groupBaseDn = groupform.groupBaseDn.value
         if (groupform.groupBaseFilter.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.groupBaseFilter = groupform.groupBaseFilter.value
         if (groupform.groupClassNames.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.groupClassNames = groupform.groupClassNames.value
         if (groupform.groupGroupname.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.groupName = groupform.groupGroupname.value
@@ -159,19 +176,19 @@
         if (groupform.groupMember.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.member = groupform.groupMember.value
         console.log('Results after Group Information is completed.')
         console.log(openLdapSimpleBindTemplate)
-        $('#output').html(JSON.stringify(openLdapSimpleBindTemplate,null,2))
-        $('#misc-tab').tab('show') 
+        $('#output').html(JSON.stringify(openLdapSimpleBindTemplate, null, 2))
+        $('#misc-tab').tab('show')
     })
 
 
-    $('#miscFormSubmit').on('click', function(){
+    $('#miscFormSubmit').on('click', function () {
         if (miscform.membersRetrievalPageSize.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.membersRetrievalPageSize = miscform.membersRetrievalPageSize.value
         if (miscform.rangeRetrieval.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.rangeRetrieval = miscform.rangeRetrieval.value
         if (miscform.serverSideSorting.value) openLdapSimpleBindTemplate.configEntities.identityStore.identityStoreSchemaType.serverSideSorting = miscform.serverSideSorting.value
         console.log('Results after Misc Information is completed.')
         console.log(openLdapSimpleBindTemplate)
-        $('#output').html(JSON.stringify(openLdapSimpleBindTemplate,null,2))
-        $('#finalize-tab').tab('show') 
+        $('#output').html(JSON.stringify(openLdapSimpleBindTemplate, null, 2))
+        $('#finalize-tab').tab('show')
     })
 
 
